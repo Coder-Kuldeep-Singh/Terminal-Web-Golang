ee73a5e9f4d37047c63fe92cbc00b1bb55a68e12
mavensingh <kuldeep.avsar@gmail.com>
2020-04-01 16:41:11 +0530

    uuid file to show data into web

---
+++ uuid/uuid.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	for {
+		time.Sleep(time.Millisecond * 500)
+		fmt.Printf("Generating a new UUID -- %s", uuid.New())
+		fmt.Println()
+	}
+}
